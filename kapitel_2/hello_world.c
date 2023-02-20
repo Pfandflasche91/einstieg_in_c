db7e80c303d1e59d987840ebeb552eeb16516d55
Max <max.alt@gmx.de>
2023-02-20 17:38:37 +0100

    dsfgasdf

--- kapitel_2/hello_world.c
+++ kapitel_2/hello_world.c
@@ -3,7 +3,8 @@
 int main(){
     int test = 1;
     printf("Hallo Welt\n");
-    printf("das ist ein test hier hier hier");
-    printf("hier bin ichasdfasdfasdf")
+    printf("das ist ein tdsafgdsafgsdfgest hiqwerqwerqwerqwerer hier hier");
+    printf("hier bin ichasdfasdfasdf");
+    asdfwerwer
     return 0;
 }
